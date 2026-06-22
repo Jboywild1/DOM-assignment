26ede1b62c9b060311334e1c8e53eff492352777
Jboywild1 <jboysamuel08@gmail.com>
2026-06-21 19:20:57 -0700

    Comment out newList function in new.js
    Comment out the newList function and its invocation.

--- new.js
+++ new.js
@@ -1,18 +1,18 @@
-function newList() {
-  let randomText = Math.random() * 10;
-  if (randomText <= 3){
-    randomText = 'Dave Concerts';
-  }else if(randomText <= 7){
-    randomText = 'Netflix and chill';
-  }else {
-    randomText = 'Generational Wealth';
-  };
+// function newList() {
+//   let randomText = Math.random() * 10;
+//   if (randomText <= 3){
+//     randomText = 'Dave Concerts';
+//   }else if(randomText <= 7){
+//     randomText = 'Netflix and chill';
+//   }else {
+//     randomText = 'Generational Wealth';
+//   };
 
-  li = document.createElement("P");
-  const liText = document.createTextNode(randomText);
-  li.appendChild(liText);
+//   li = document.createElement("P");
+//   const liText = document.createTextNode(randomText);
+//   li.appendChild(liText);
 
-  document.body.appendChild(liText);
-}
+//   document.body.appendChild(liText);
+// }
 
-newList();
+// newList();
